d7d81386dcad0212c8d1bd23f1419527a9840ef1
Wiibleyde <nathan@bonnell.fr>
2023-10-10 10:55:51 +0200

    CalculatorScreen

---
+++ CalculatorScreen.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CalculatorQuest;
+
+public partial class CalculatorScreen : Window
+{
+    public CalculatorScreen()
+    {
+        InitializeComponent();
+    }
+}
